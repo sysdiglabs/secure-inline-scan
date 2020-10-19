34ce4c4eba7d71f2a007bc0fc66dab73750891aa
Alvaro Iradier <airadier@gmail.com>
2020-10-19 21:08:56 +0200

    Fix the image name in README.md and inline_scan_docker.sh

--- inline_scan_docker.sh
+++ inline_scan_docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -ti -v /var/run/docker.sock:/var/run/docker.sock sysdiglabs/sysdig-inline-scan "$@"
+docker run --rm -ti -v /var/run/docker.sock:/var/run/docker.sock sysdiglabs/secure-inline-scan:2 "$@"
