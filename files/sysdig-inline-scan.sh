c051770824afe78ff8afbe148c9e8dbd158f4f2c
Álvaro Iradier <alvaro.iradier@sysdig.com>
2020-11-13 14:52:47 +0100

    Add small explanation to -r (download PDF) flag (#83)
    * Add small explanation to -r (download PDF) flag
    
    * Minor documentation fix
    
    Co-authored-by: Mateo Burillo <mateobur@gmail.com>

--- files/sysdig-inline-scan.sh
+++ files/sysdig-inline-scan.sh
@@ -100,7 +100,8 @@ Sysdig Inline Analyzer -- USAGE
     -d <SHA256> [optional] Specify image digest (ex: -d 'sha256:<64 hex characters>')
                 Alias: --digest
     -c          [optional] Remove the image from Sysdig Secure if the scan fails
-    -r <PATH>   [optional] Download scan result pdf in a specified local directory (ex: -r /staging/reports)
+    -r <PATH>   [optional] Download scan result pdf in a specified container-local directory (ex: -r /staging/reports)
+                This directory needs to be previously mounted from the host to persist the data
                 Alias: --report-folder
     -v          [optional] Increase verbosity
                 Alias: --verbose
