f0832e5afaed5c0493e5e684e170d964e7721887
Alvaro Iradier <airadier@gmail.com>
2020-11-25 13:56:38 +0100

    Remove files to keep this repo with deprecation warning

--- examples/tekton/beta/service-role.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubectl create clusterrole tutorial-role \
-               --verb=* \
-               --resource=deployments,deployments.apps
-
-kubectl create clusterrolebinding tutorial-binding \
-             --clusterrole=tutorial-role \
-             --serviceaccount=default:tutorial-service
